b3ba7f0845c33cb9acd7e4df4e68b471625de2ab
vishal <vishal@sapient.com>
2019-09-04 16:28:22 +0000

    DISK test file update

--- apache/tasks/script.sh
+++ apache/tasks/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-> /opt/log/httpd/new_httpd.log
+#> /var/log/httpd/new_httpd_log
 for (( i = 5; i >=0; i-- )) ; do
-     grep $(date +%R -d "-$i  min ") /var/log/httpd/error.log  >>  /opt/log/httpd/new_httpd.log
+     grep $(date +%R -d "-$i  min ") /var/log/httpd/error_log
 done
